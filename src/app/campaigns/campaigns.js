091324cdae8a7eb3e6e86ce0e7a596dfd3381332
Jose-silvaa <jvitorfacanha@gmail.com>
2024-10-27 21:43:06 -0300

    Meio da campanha

--- src/app/campaigns/campaigns.js
+++ src/app/campaigns/campaigns.js
@@ -2,7 +2,9 @@ export default function Campaign(){
     return (
         <>
             <h1>Esse é o começo da campanha</h1>      
+            <h3>Meio da Campanha</h3>
             <h5>Final da Campanha</h5>  
+            
         </>
     )
 }
