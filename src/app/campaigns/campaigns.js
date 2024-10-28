23479c09e4028fd8963887af983834b3a4d6d3bc
José Vitor Façanha da Silva <jvitorfacanha@gmail.com>
2024-10-27 21:39:45 -0300

    Add campaigns details

---
+++ src/app/campaigns/campaigns.js
@@ -0,0 +1,8 @@
+export default function Campaign(){
+    return (
+        <>
+            <h1>Esse é o começo da campanha</h1>      
+            <h5>Final da Campanha</h5>  
+        </>
+    )
+}
