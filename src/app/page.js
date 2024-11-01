b238553a7a287f50ff69f712c4effcaf338cdcaf
Jose-silvaa <jvitorfacanha@gmail.com>
2024-11-01 20:23:21 -0300

    Finish header page inbox

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Header from "@/components/header/Header";
 export default function Home() {
   return (
     <main>
+      <Header />
       <section className="section__message">
         <h2>Empower your clients to manage their marketing campaigns, so you can focus on growth.</h2>
         <p>Streamline your workflow while giving clients the control they need to succeed</p>
