32f4785d9c735ae720ab543a92ad06005a78e905
Jose-silvaa <jvitorfacanha@gmail.com>
2024-11-03 21:07:38 -0300

    Pagina de login modificada

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -18,32 +18,28 @@ export default function Login(){
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        console.log(loginData);
-
         setLoginData({email : "", password : ""})
     }
 
     return(
-        <>
-          <section>
-            <h2>Welcome Back to FlowTarget</h2>
-            <p>Enter your email and password to acess your account</p>
-          </section>
-          <section className="section__login">
-                <form className="form__login" onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" id="email" name="email" value={loginData.email} onChange={handleChange} required placeholder="Enter your email"></input>
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" value={loginData.password} onChange={handleChange} required placeholder="Enter your password"></input>
-                </div>
-                <div>
-                    <button type="submit">Sign In</button>
-                 </div>
-            </form>
-         </section>
-        </>
+        <div className="box">
+            <section className="box__login">
+                <h2>Log in to your Account</h2>
+                <p>Enter your email and password to acess your account</p>
+            </section>
+            <section className="section__login">
+                    <form className="form__login" onSubmit={handleSubmit}>
+                    <div>
+                        <input type="text" id="email" name="email" value={loginData.email} onChange={handleChange} required placeholder="Email"></input>
+                    </div>
+                    <div>
+                        <input type="password" id="password" name="password" value={loginData.password} onChange={handleChange} required placeholder="Password"></input>
+                    </div>
+                    <div>
+                        <button type="submit">Log in</button>
+                    </div>
+                </form>
+            </section>
+        </div>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import "./main.css"
 export default function Home() {
   return (
     <main>
-      <Header />
       <section className="section__message">
         <h2>EMPOWER YOUR CLIENTS TO MANAGE THE MARKETING CAMPAIGNS</h2>
         <button className="button__main">GET FLOW TARGET</button>
